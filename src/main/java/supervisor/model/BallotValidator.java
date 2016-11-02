1d3a8331ef5af79b718167ffb18c90827318e3e7
alexshultz <alexshultz1@gmail.com>
2016-11-02 15:32:28 -0500

    added spoiler after 5 minutes w/o being cast

---
+++ src/main/java/supervisor/model/BallotValidator.java
@@ -0,0 +1,13 @@
+package supervisor.model;
+
+import java.util.TimerTask;
+
+/**
+ * Created by alexshultz on 11/2/16.
+ */
+public class BallotValidator extends TimerTask {
+
+    public void run(){
+
+    }
+}
